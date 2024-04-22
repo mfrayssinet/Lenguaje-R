cf03233d436be6675ab934ebcc30050d1e4e544f
mfrayssinet <83352917+mfrayssinet@users.noreply.github.com>
2024-04-21 23:12:00 -0500

    Create demo3.r

---
+++ demo3.r
@@ -0,0 +1,9 @@
+# Demo3: Calcular la diferencia, producto y cociente de dos números
+numero1 <- 10
+numero2 <- 3
+diferencia <- numero1 - numero2
+producto <- numero1 * numero2
+cociente <- numero1 / numero2
+print(paste("Diferencia:", diferencia))
+print(paste("Producto:", producto))
+print(paste("Cociente:", cociente))
