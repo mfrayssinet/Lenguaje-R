984ea75f21e96c40755f843e5305006a9da892f2
mfrayssinet <83352917+mfrayssinet@users.noreply.github.com>
2024-04-21 23:13:53 -0500

    Create demo6.r

---
+++ demo6.r
@@ -0,0 +1,5 @@
+# Demo6: Crear un vector de caracteres con cinco nombres de frutas y usar un loop para imprimir cada uno
+frutas <- c("manzana", "banana", "cereza", "durazno", "mango")
+for(fruta in frutas) {
+  print(fruta)
+}
